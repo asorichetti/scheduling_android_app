82377068f8a0c5da59eea4c441cd2b5459f67458
Alex Sorichetti <107128425+asorichetti@users.noreply.github.com>
2025-11-12 15:19:24 -0500

    Comment Header Update

--- SchedulingApp/SchedulingApp/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/SchedulingApp.AssemblyInfo.cs
+++ SchedulingApp/SchedulingApp/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/SchedulingApp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchedulingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+2ee331266d7b50353ff8255320ed851c5ab4bb83")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchedulingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchedulingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
